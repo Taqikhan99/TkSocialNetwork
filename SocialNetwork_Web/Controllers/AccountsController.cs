8e494b278fa40023bfe28196611f98bebb0b53fc
Taqi Ahmed <taqiahmed339@gmail.com>
2022-12-18 17:32:49 +0500

    Add project files.

---
+++ SocialNetwork_Web/Controllers/AccountsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialNetwork_Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //Login method
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        //signup method
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+    }
+}
